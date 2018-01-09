35c5e99124181567473b6db6eb120e5e0c671b73
Julien Lhermitte <lhermitte@bnl.gov>
2018-01-09 14:34:27 -0500

    Update to use nslsii tools

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -1,64 +1,10 @@
-# Import matplotlib and put it in interactive mode.
-import matplotlib
-matplotlib.use('Qt5Agg')
-import matplotlib.pyplot as plt
-plt.ion()
-
-# Make ophyd listen to pyepics.
-from ophyd import setup_ophyd
-setup_ophyd()
-
-# Set up a RunEngine and use metadata backed by a sqlite file.
-from bluesky import RunEngine
-from bluesky.utils import get_history
-RE = RunEngine(get_history())
-
-# Set up a Broker.
-from databroker import Broker
-db = Broker.named('cms')
-
-# Subscribe metadatastore to documents.
-# If this is removed, data is not saved to metadatastore.
-RE.subscribe(db.insert)
-
-# Set up SupplementalData.
-from bluesky import SupplementalData
-sd = SupplementalData()
-RE.preprocessors.append(sd)
-
-# Add a progress bar.
-from bluesky.utils import ProgressBarManager
-pbar_manager = ProgressBarManager()
-RE.waiting_hook = pbar_manager
-
-# Register bluesky IPython magics.
-from bluesky.magics import BlueskyMagics
-get_ipython().register_magics(BlueskyMagics)
-
-# Set up the BestEffortCallback.
-from bluesky.callbacks.best_effort import BestEffortCallback
-bec = BestEffortCallback()
-RE.subscribe(bec)
-peaks = bec.peaks  # just as alias for less typing
+import nslsii
+nslsii.configure_base(get_ipython().user_ns, 'cms')
 
 # At the end of every run, verify that files were saved and
 # print a confirmation message.
 from bluesky.callbacks.broker import verify_files_saved
 # RE.subscribe(post_run(verify_files_saved), 'stop')
-
-# Make plots update live while scans run.
-from bluesky.utils import install_qt_kicker
-install_qt_kicker()
-
-# Optional: set any metadata that rarely changes.
-# RE.md['beamline_id'] = 'YOUR_BEAMLINE_HERE'
-
-# convenience imports
-from bluesky.callbacks import *
-from bluesky.callbacks.broker import *
-from bluesky.simulators import *
-from bluesky.plans import *
-import numpy as np
 
 from pyOlog.ophyd_tools import *
 
