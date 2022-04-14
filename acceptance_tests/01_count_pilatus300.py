b52f9710d01c0f82918ad77b93943af76fe50d9a
CMS Operator <xf11bm@xf11bm-ws1.nsls2.bnl.local>
2022-04-14 11:46:01 -0400

    acceptance/01>>>>>

--- acceptance_tests/01_count_pilatus300.py
+++ acceptance_tests/01_count_pilatus300.py
@@ -1,4 +1,13 @@
 from bluesky.plans import count
+from bluesky.plans import bps
 
+def pil_test():
+    uid = RE(count([pilatus2M]))
+    h=db[uid[0]]
+    # print(uid)
+    print(h.start)
+    return h
 
-RE(count([pilatus300]))
+def motor_test(pos=0):
+    RE(bps.mov(smx, pos))
+
