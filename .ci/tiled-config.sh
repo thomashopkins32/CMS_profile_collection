8711f8bed9410e821b43af63d3fa058f7017b315
Max Rakitin <mrakitin@bnl.gov>
2025-02-07 10:52:41 +0100

    CI: add a script for tiled config - `.ci/tiled-config.sh`

---
+++ .ci/tiled-config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Tiled profile config:
+
+tiled_profiles_dir="$HOME/.config/tiled/profiles/"
+beamline_acronym="cms"
+
+mkdir -v -p ${tiled_profiles_dir}
+
+cat << EOF > ${tiled_profiles_dir}/profiles.yml
+${beamline_acronym:-local}:
+  direct:
+    authentication:
+      allow_anonymous_access: true
+    trees:
+    - tree: databroker.mongo_normalized:Tree.from_uri
+      path: /
+      args:
+        uri: mongodb://localhost:27017/metadatastore-local
+        asset_registry_uri: mongodb://localhost:27017/asset-registry-local
+EOF
+
+cat ${tiled_profiles_dir}/profiles.yml
