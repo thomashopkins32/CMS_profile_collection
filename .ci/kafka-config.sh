c3e859fe0c686b4d96e62e5d927bc6ff328d6ab9
Max Rakitin <mrakitin@bnl.gov>
2025-02-11 17:40:48 -0500

    CI: add missing configs

---
+++ .ci/kafka-config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Kafka config:
+cat << EOF > kafka.yml
+---
+abort_run_on_kafka_exception: false
+bootstrap_servers:
+    - localhost:9092
+runengine_producer_config:
+    security.protocol: PLAINTEXT
+EOF
+
+sudo mkdir -v -p /etc/bluesky/
+sudo mv -v kafka.yml /etc/bluesky/kafka.yml  # TODO: put it into the home directory via an env var
+cat /etc/bluesky/kafka.yml
