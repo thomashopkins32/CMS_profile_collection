77a910847dbbc44ca4215e083eb353c2da86a947
Max Rakitin <mrakitin@bnl.gov>
2025-02-12 11:51:53 -0500

    CI: more diag checks of the redis address

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+set -veo pipefail
+
 sudo echo "127.0.0.1 info.cms.nsls2.bnl.gov" >> /etc/hosts
+cat /etc/hosts
 
 mkdir -v -p ~/.ipython/profile_test/startup/
 cp -v startup/.cms_config ~/.ipython/profile_test/startup/
