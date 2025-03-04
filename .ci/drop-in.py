85a445661115753890783ec76ed9b3eaf1df038d
Noah van der Vleuten <54477889+NoahVl@users.noreply.github.com>
2025-03-04 16:07:38 -0500

    Add scan_id key to RE to prevent KeyError (#2)
    * Add scan_id key to RE to prevent KeyError
    
    * Change scan_id to 1

--- .ci/drop-in.py
+++ .ci/drop-in.py
@@ -5,6 +5,8 @@ pilatus2M.tiff.create_directory.set(-20)
 sam = SampleGISAXS('test')
 detselect(pilatus2M)
 
+RE.md.update({'scan_id': 1})
+
 pilatus2M.cam.num_images.put(1)
 
 uid, = RE(bp.count([pilatus2M], num=3))
